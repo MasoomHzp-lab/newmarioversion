4d83a42d671f0995edc584b644312c045956bb2c
Bahar <susieshooter@gmail.com>
2025-10-07 11:52:33 +0330

    updATE the bugs

--- Assets/Scripts/GoalTrigger.cs
+++ Assets/Scripts/GoalTrigger.cs
@@ -8,12 +8,11 @@ public class GoalTrigger : MonoBehaviour
 void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
-        {
-            Debug.Log("🎉 برنده شدی!");
-            // برای مثال، می‌تونی صحنه بعدی رو لود کنی
-            // یا یه پیام نشون بدی:
-            SceneManager.LoadScene("WinScene"); // اسم صحنه بردت رو بزن
-        }
+{
+    Debug.Log("🎉 برنده شدی!");
+    FindObjectOfType<GameManageer>().Win();
+}
+
     }
 
 }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -33,8 +33,8 @@ public class PlayerHealth : MonoBehaviour
     }
 
     void GameOver()
-    {
-        Debug.Log("💀 Game Over!");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+{
+    Debug.Log("💀 Game Over!");
+    FindObjectOfType<GameManageer>().GameOver();
+}
 }
